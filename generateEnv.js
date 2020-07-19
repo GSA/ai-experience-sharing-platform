947b04278b6daa33819e97a129c583b962cdaaab
Tim Plummer <tplummer@flexion.us>
2020-07-19 12:59:53 -0500

    Update generateEnv.js

--- generateEnv.js
+++ generateEnv.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const PUBLIC_URL = `PUBLIC_URL=${process.env.BASEURL}`;
 const ENV = path.join(__dirname, ".env");
-if (fs.exists(ENV)) {
+if (fs.existsSync(ENV)) {
   console.log("ENV file exists. Appending...");
   fs.appendFileSync(ENV, `\r\n \r\n`);
   fs.appendFileSync(ENV, PUBLIC_URL);
