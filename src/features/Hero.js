b8df18529bad545bbdbbf79fd606d09d45f7b385
Tim Plummer <tplummer@flexion.us>
2020-07-21 19:16:54 -0500

    update hero component

--- src/features/Hero.js
+++ src/features/Hero.js
@@ -1,21 +1,19 @@
-import React from 'react';
+import React from "react";
 
 /*
   This will be displayed on the homepage. Ideally, you want to highlight key goals of the website
 */
 
-const Hero = ({ children }) => (
-  <section className="usa-hero">
-    <div className="grid-container">
-      <div className="grid-row">
-        <div className="grid-col-4 grid-offset-8">
-          <h2 className="usa-hero__heading">
-            <span className="usa-hero__heading--alt">{children}</span>
-          </h2>
-        </div>
-      </div>
-    </div>
-  </section>
-);
+const Hero = ({ children, background }) => {
+  const style = {};
+  if (background) {
+    style.backgroundImage = `url("${background}")`;
+  }
+  return (
+    <section className="usa-hero" style={style}>
+      {children}
+    </section>
+  );
+};
 
 export default Hero;
