1da371a1c12f774e8b3b6a31cc8098f49e7b90aa
Tim Plummer <tplummer@flexion.us>
2020-07-21 19:40:37 -0500

    Update Hero.js

--- src/features/Hero.js
+++ src/features/Hero.js
@@ -7,7 +7,7 @@ import React from "react";
 const Hero = ({ children, background }) => {
   const style = {};
   if (background) {
-    style.backgroundImage = `url('${process.env.PUBLIC_URL}/images/${background})`;
+    style.backgroundImage = `url('${process.env.PUBLIC_URL}/images/${background}')`;
   }
   return (
     <section className="usa-hero" style={style}>
