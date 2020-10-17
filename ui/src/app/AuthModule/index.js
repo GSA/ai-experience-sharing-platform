dc8692fe829584679266295909c5852d8c5cb5d4
Tim Plummer <timothy.plummer@gsa.gov>
2020-10-17 11:10:10 -0500

    update auth module

--- ui/src/app/AuthModule/index.js
+++ ui/src/app/AuthModule/index.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import QS from "query-string";
 import context from "./context";
 
 export const initialState = {
@@ -16,6 +17,28 @@ export const login = createAsyncThunk(
       password,
     })
 );
+
+export const loginUrl = (params) => {
+  const rootUrl =
+    "https://idp.int.identitysandbox.gov/openid_connect/authorize";
+
+  const query = {
+    acr_values: "http://idmanagement.gov/ns/assurance/ial/1",
+    client_id: "urn:gov:gsa:openidconnect.profiles:sp:sso:gsa:ai_experience",
+    nonc:
+      "66A27845-DB89-4433-9AA3-B4B21257FAE9FD69096A-22B2-4B3F-8EE6-0E85B5E00307",
+    prompt: "select_account",
+    redirect_uri:
+      "https://strapi-api-host-dev.app.cloud.gov/connect/logingov/callback",
+    response_type: "code",
+    scope: "openid+email",
+    state:
+      "abcdefghijklmnopabcdefghijklmnopFD69096A-22B2-4B3F-8EE6-0E85B5E00307",
+    ...params,
+  };
+
+  return `${rootUrl}?${QS.stringify(query)}`;
+};
 
 export const logout = createAsyncThunk(
   "auth/logout",
--- ui/src/features/Login/index.js
+++ ui/src/features/Login/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Button from "components/Button";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import FourOhFour from "templates/FourOhFour";
+import { loginUrl } from "app/AuthModule";
 import loginLogo from "./logingov.svg";
 
 const Login = ({ children, oAuthUrl }) => {
-  const dispatch = useDispatch();
   const { isAuth, error } = useSelector((state) => state.auth);
 
   if (error) {
@@ -26,8 +26,7 @@ const Login = ({ children, oAuthUrl }) => {
 };
 
 Login.defaultProps = {
-  oAuthUrl:
-    "https://idp.int.identitysandbox.gov/openid_connect/authorize?acr_values=http%3A%2F%2Fidmanagement.gov%2Fns%2Fassurance%2Fial%2F1&client_id=urn:gov:gsa:openidconnect.profiles:sp:sso:gsa:ai_experience&nonce=66A27845-DB89-4433-9AA3-B4B21257FAE9FD69096A-22B2-4B3F-8EE6-0E85B5E00307&prompt=select_account&redirect_uri=https%3A%2F%2Fstrapi-api-host-dev.app.cloud.gov%2Fconnect%2Flogingov%2Fcallback&response_type=code&scope=openid+email&state=abcdefghijklmnopabcdefghijklmnopFD69096A-22B2-4B3F-8EE6-0E85B5E00307",
+  oAuthUrl: loginUrl(),
 };
 Login.propTypes = {
   children: PropTypes.node,
