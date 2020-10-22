75e05fa47150e7180371f13103b81a2480cc8c50
Taylor Zajicek <tzajicek@flexion.us>
2020-10-22 11:45:07 -0500

    Add a index.htmlf for specs.

--- cms/tests/helpers/strapi.js
+++ cms/tests/helpers/strapi.js
@@ -1,11 +1,24 @@
+const fs = require('fs').promises;
+const path = require('path');
 const Strapi = require('strapi');
 const http = require('http');
 
 let instance;
 
+const fakeSpaIndex = '<html><head><title>Fake SPA Index</title></head><body></body></html>';
+
+const setupFakeSpaIndex = async () => {
+  const publicDir = path.join(__dirname, '../../public')
+  try {
+    await fs.mkdir(publicDir);
+    await fs.writeFile(path.join(publicDir, 'index.html', fakeSpaIndex, { flag: 'wx' }))
+  } catch (err) {}
+};
+
 async function setupStrapi() {
   if (!instance) {
     jest.setTimeout(32 * 1000);
+    await setupFakeSpaIndex();
     /** the following code in copied from `./node_modules/strapi/lib/Strapi.js` */
     await Strapi().load();
     instance = strapi; // strapi is global now
