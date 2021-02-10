98d37750249954725ea68c07bfad2b5d352e97f9
Taylor Zajicek <tzajicek@flexion.us>
2021-02-10 13:15:47 -0600

    Testing feedback 2

--- ui/src/components/Date/index.js
+++ ui/src/components/Date/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const RDate = ({ format, children }) => {
-  return <span>{new Intl.DateTimeFormat('en-US', {dateStyle: format}).format(Date.parse(children))}</span>;
+  try {
+    return <span>{new Intl.DateTimeFormat('en-US', {dateStyle: format}).format(Date.parse(children))}</span>;
+  } catch (e) {
+    return null;
+  }
 };
 
 Date.defaultProps = {
