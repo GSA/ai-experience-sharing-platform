f86f3cc7de245744ee0973e4c2f166bb162cff4f
Taylor Zajicek <tzajicek@flexion.us>
2021-05-26 15:26:36 -0500

    AI-381_increase_memory_logging

--- deployment/modules/space/main.tf
+++ deployment/modules/space/main.tf
@@ -83,5 +83,7 @@ resource "cloudfoundry_app" "strapi-api-host" {
   }
   environment = {
     CMSURL = "https://${var.strapi_hostname}.${data.cloudfoundry_domain.app.name}"
+    WEB_CONCURRENCY = "4"
+    LOG_CONCURRENCY = "true"
   }
 }
