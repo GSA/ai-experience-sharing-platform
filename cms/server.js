d3c3be7b8cf69296e16db308b929548dc992e151
Taylor Zajicek <tzajicek@flexion.us>
2021-06-09 14:05:56 -0500

    Make TTFB faster

--- cms/server.js
+++ cms/server.js
@@ -1,3 +1,32 @@
 const strapi = require('strapi');
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const port = process.env.PORT || 8080;
+const strapiPort = (parseInt(port, 10) + 1).toString();
+
+const app = express();
+const strapiProxy = createProxyMiddleware({ target: `http://localhost:${strapiPort}` });
+
+const strapiRoutes = [
+  "admin",
+  "auth",
+  "connect",
+  "content-manager",
+  "content-type-builder",
+  "users-permissions",
+  "_health",
+  "api",
+  "upload-auth",
+  "upload",
+  "logingov-admin",
+];
+for (const route of strapiRoutes) {
+  app.use(`/${route}*`, strapiProxy);
+}
+app.use(express.static('public'));
+app.use('*', strapiProxy);
+app.listen(port);
+
+process.env.PORT = strapiPort;
 strapi().start();
